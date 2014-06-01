f5b87021c91b3964d187ffbe59a2afef47c2a78e
jgerardsimcock <justin_simcock@yahoo.com>
2014-05-31 16:23:16 -1000

    Final

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -1,5 +1,91 @@
 (function(){
 
-  
+// Question 1
 
+  var score = 0;
+  var answer = prompt("Where are the biggest waves in Oahu?");
+  
+  if (answer == "Waimea") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+// Question 2
+
+ var answer = prompt("What is the freaky wave called near Portlock?");
+  
+  if (answer == "China Walls") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+// Question 3
+
+ var answer = prompt("What is the middle wodden spine of a surfboard called?");
+  
+  if (answer == "Stringer") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+// Question 4
+
+  var answer = prompt("What is a surfboard made of?");
+  
+  if (answer == "Foam") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+// Question 5
+
+  var answer = prompt("What are young surfers called?");
+  
+  if (answer == "Groms") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+// Question 6
+
+  var answer = prompt("What are young surfers called?");
+  
+  if (answer == "Groms") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+  // Question 7
+
+  var answer = prompt("What are young surfers called?");
+  
+  if (answer == "Groms") {
+    alert("Right!");
+    ++score;
+  }
+  else {
+    alert("Sorry, try again");
+  }
+
+  alert(score);
 })(); 
+
+// var question ["Where are biggest waves in Oahu?", "What is the freaky wave called near Portlock?", "What is the name of the beach park where Pipeline is located?", "What is a surfboard made of?", "What is the middle wodden spine of a surfboard called?", "What are young surfers called?"]
